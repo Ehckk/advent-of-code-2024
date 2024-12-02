8fda96c13a3407b70b6a29d4db0ccf55cab42a2b
eric.miguel <eric.miguel@dxtcommodities.com>
2024-12-02 10:05:35 -0500

    day01 part1 solution

--- day01/part1.py
+++ day01/part1.py
@@ -2,8 +2,14 @@ from util import get_input
 
 
 def solve():
-	puzzle_input = get_input(1, 1)
-	print(puzzle_input)
+	puzzle_input = list(map(lambda line: line.split("   "), get_input(1, 1).splitlines()))
+	distances = list(map(lambda pair: tuple(map(int, pair)), puzzle_input))
+	left_dist = list(sorted(map(lambda pair: pair[0], distances)))
+	right_dist = list(sorted(map(lambda pair: pair[1], distances)))
+	total = 0
+	for i in range(max(len(left_dist), len(right_dist))):
+		total += abs(left_dist[i] - right_dist[i])
+	print(total)
 
 
 if __name__ == "__main__":
